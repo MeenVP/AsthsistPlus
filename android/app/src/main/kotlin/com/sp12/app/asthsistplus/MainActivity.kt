e3ab513c0084916f403d1cf4b3f347bbea577236
Viphu Sopanakitkosol <viphu.sop@student.mahidol.ac.th>
2024-05-01 20:32:35 +0700

    Renamed package

---
+++ android/app/src/main/kotlin/com/sp12/app/asthsistplus/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.sp12.app.asthsistplus
+
+
+import io.flutter.embedding.android.FlutterActivity
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
